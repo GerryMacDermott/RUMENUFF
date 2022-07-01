ed4d2bf6eaf27a0ef818109caf49160350f6c469
GerryMacDermott <41880873+GerryMacDermott@users.noreply.github.com>
2022-07-01 15:17:51 +1000

    Update DCQ.js

--- DCQ.js
+++ DCQ.js
@@ -8,6 +8,7 @@ var height = 1; //number of guesses
 var width = word.length; //length of the word
 var GuessTile = word.length + 1;
 var col = word.length;
+var colFINAL = 0;
 var row = 0;
 var done = 0;
 String: sentence = "Hello";
@@ -15,7 +16,11 @@ var failcount = 1;
 letterinput = 0;
 var attempts = word.length + 1;
 var errorcount = 0;
-var guessed = [word.length];
+var guessed = [];
+var letterwasguessedtrue = [];
+var doneFINAL = 0;
+var GOGOFINALGUESS = 1;
+guessed.length = GuessTile;
 const FG = document.querySelectorAll(".finalguess1 button");
 
 var GameOver = false;
@@ -23,6 +28,8 @@ var GameOver = false;
 window.onload = function () {
     initilise();
 }
+
+
 
 function initilise() {
     //Present Question --> need API or way to pull questions?
@@ -45,94 +52,67 @@ function initilise() {
     Gtile.innerText = "";
     document.getElementById("letter").appendChild(Gtile);
 
-    //Listen for Key Press
-    document.addEventListener("keyup", (e) => {
-        if (GameOver) return;
-        if ("KeyA" <= e.code && e.code <= "KeyZ") {
-            if (col < GuessTile) {
+    //On-screen keyboard letter recognition
+    const keys = document.querySelectorAll(".keyboard-row button");
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const keypressed = target.getAttribute("data-key");
+
+
+            Enter: if (keypressed == "enter" && col == GuessTile) {
                 let CurrentTile = document.getElementById(row.toString + '-' + GuessTile.toString());
-                if (CurrentTile.innerText == "") { //this may be redundant if statement
-                    CurrentTile.innerText = e.code[3];
-                    col += 1;
+                for (let cycle = 0; cycle <= GuessTile; cycle++) {
+                    if (CurrentTile.innerText == guessed[0, cycle]) {
+                        document.getElementById("answer").innerText = "You have already tried this letter!";
+                        col -= 1;
+                        let CurrentTile = document.getElementById(row.toString + '-' + GuessTile.toString());
+                        CurrentTile.innerText = "";
+                        break Enter;
+                    }
                 }
+                guessed[0, attempts] = CurrentTile.innerText;
+                update();
+            };
+
+            if (keypressed == "del" && col == GuessTile) {
+                col -= 1;
+                let CurrentTile = document.getElementById(row.toString + '-' + GuessTile.toString());
+                CurrentTile.innerText = "";
+                return;
+            };
+
+            if (col == GuessTile - 1 && keypressed != "del" && keypressed != "enter") {
+                let CurrentTile = document.getElementById(row.toString + '-' + GuessTile.toString());
+                CurrentTile.innerText = keypressed;
+                col += 1;
             }
-        }
+        };
+    };
+    let CurrentTile = document.getElementById(row.toString + '-' + GuessTile.toString());
 
-        else if (e.code == "Backspace" && col == GuessTile) {
-            col -= 1;
-            let CurrentTile = document.getElementById(row.toString + '-' + GuessTile.toString());
-            CurrentTile.innerText = "";
-        }
 
-        Enter: if (e.code == "Enter" && col == GuessTile) {
-            let CurrentTile = document.getElementById(row.toString + '-' + GuessTile.toString());
-            for (let cycle = 0; cycle <= GuessTile; cycle++) {
-                if (CurrentTile.innerText == guessed[cycle]) {
-                    document.getElementById("answer").innerText = "You have already tried this letter!";
-                    col -= 1;
-                    let CurrentTile = document.getElementById(row.toString + '-' + GuessTile.toString());
-                    CurrentTile.innerText = "";
-                    break Enter;
-                }
-            }
-
-            guessed[0, attempts] = CurrentTile.innerText;
-            update();
-
-        }
-        else if (e.code == "Enter") { //maybe redundant
-            document.getElementById("answer").innerText = "bing bong " + errorcount;
-            errorcount += 1;
-        }
-
-    })
     //document.getElementsByName("FGbutton").onclick = function() {test()};
     var FG = document.getElementById("FGbutton");
     FG.onclick = function () { FINALATTEMPT() };
+
+
 }
-
-function FINALATTEMPT() {
-    //check for button click
-    var FG = document.getElementById("FGbutton");
-                let finalword = document.getElementById("guessword").value;
-                console.log(finalword);
-                finalwordcheck = finalword.toUpperCase();
-                if (finalwordcheck == word){
-                    GameOver = true;
-                    document.getElementById("answer").innerText = "You Win\n" + failcount + " attempts";
-                    FG.classList.toggle("win");
-                    for (let d = 0; d < width; d++) {
-                        let CurrentTile = document.getElementById(row.toString + '-' + d.toString()); //may be redundant
-                        CurrentTile.innerText = word[d];
-                    }
-                }
-                if (finalwordcheck != word){
-                    GameOver = true;
-                    document.getElementById("answer").innerText = "You Lose dumb dumb\n The correct answer was\n" + word;
-                    FG.classList.toggle("lose");
-                    for (let d = 0; d < width; d++) {
-                        let CurrentTile = document.getElementById(row.toString + '-' + d.toString()); //may be redundant
-                        CurrentTile.innerText = word[d];
-                    }
-                }
-}
-
-
 //Check letter is present
 function update() {
     //need a if attempts = 0 --> text on screen final guess!!
     for (let d = 0; d < width; d++) {
         let CurrentTile = document.getElementById(row.toString + '-' + GuessTile.toString()); //may be redundant
-        let letter = CurrentTile.innerText
+        let lettercheck = CurrentTile.innerText
 
-        if (word[d] == letter) {
+        //check letter is in word
+        if (word[d] == lettercheck) {
+            //if let is present display it
             let CurrentTile = document.getElementById(row.toString + '-' + d.toString());
-            CurrentTile.innerText = letter; //maybe make 'letter'?
+            CurrentTile.innerText = lettercheck;
             done += 1;
         }
     }
-
-
+    //check word is completed
     if (done == width) {
         GameOver = true;
         document.getElementById("answer").innerText = "You Win\n" + failcount + " attempts";
@@ -147,67 +127,70 @@ function update() {
     }
 }
 
-
-/*   //Final Guess
-  document.addEventListener("keyup", (e) =>{
-    if (GameOver) return;
-
-
-    if ("KeyA" <= e.code && e.code <= "KeyZ"){
-        if (col < width) {
-            let CurrentTile = document.getElementById(row.toString + '-' + col.toString());
-            if (CurrentTile.innerText == "") {
-                CurrentTile.innerText = e.code[3];
-                col += 1;
-            }
-        }
-    }
-
-    else if (e.code == "Backspace") {
-        if (0 < col && col <= width) {
-            col -= 1;
-        }
-        let CurrentTile = document.getElementById(row.toString + '-' + col.toString());
-        CurrentTile.innerText = "";
-    }
-
-    else if (e.code == "Enter" && col == width) {
-        update();
-        col = 0;
-    }
-    else if (e.code == "Enter"){
-        document.getElementById("answer").innerText = "Must be a " + width + " letter word!";
-    }
-
-})
-
-
-//Check guess is correct
-function update() {
-
-    for (let d =0; d < width; d++){
+FINALATTEMPT: function FINALATTEMPT() {
+    var FG = document.getElementById("FGbutton");
+    FG.classList.toggle("zone");
+    for (let d = 0; d < width; d++) {
         let CurrentTile = document.getElementById(row.toString + '-' + d.toString());
-        let letter = CurrentTile.innerText
-
-        if (word[d] == letter) {
-            let CurrentTile = document.getElementById(row.toString + '-' + d.toString()); 
-            CurrentTile.innerText = e.code[3];
-            done += 1;
+        if (CurrentTile.innerText != "") {
+            CurrentTile.classList.add("tilecorrect");
+            letterwasguessedtrue[0, d] = 1;
+            colFINAL += 1;
         }
+        else
+            letterwasguessedtrue[0, d] = 0;
     }
 
-    
-    if (done == width){
-        GameOver = true;
-        document.getElementById("answer").innerText = "You Win\n" + failcount + " attempts";
-    }
-    else if (done != width) {
-        done = 0;
-        document.getElementById("answer").innerText = "You have" + attempts + "left!";
-        failcount +=1;
-        for (let d =0; d < width; d++){
-            let CurrentTile = document.getElementById(row.toString + '-' + d.toString());
-            CurrentTile.innerText = "";
-        }
-    }
-} */
+    const keys = document.querySelectorAll(".keyboard-row button");
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const keypressed = target.getAttribute("data-key");
+
+
+            Enter: if (keypressed == "enter" && colFINAL == width) {
+                for (let d = 0; d < width; d++) {
+                    let CurrentTile = document.getElementById(row.toString + '-' + d.toString()); //may be redundant
+                    let lettercheck = CurrentTile.innerText
+                    if (word[d] == lettercheck) {
+                        doneFINAL += 1;
+                    }
+                }
+                if (doneFINAL == width) {
+                    document.getElementById("answer").innerText = "You Win\n" + failcount + " attempts";
+                    FG.classList.toggle("win");
+                    GameOver = true;
+                }
+                else if (doneFINAL != width) {
+                    document.getElementById("answer").innerText = "You Lose dumb dumb\n The correct answer was\n" + word;
+                    FG.classList.toggle("lose");
+                }
+            };
+
+            Delete: if (keypressed == "del" && colFINAL < GuessTile) {
+                colFINAL -= 1;
+                for (let d = width; d >= 0; d--) {
+                    let CurrentTile = document.getElementById(row.toString + '-' + d.toString());
+                    if (letterwasguessedtrue[0, d] == 2 && CurrentTile.innerText != "") {
+                        CurrentTile.innerText = "";
+                        letterwasguessedtrue[0, d] = 0;
+                        break Delete;
+                    };
+                }
+            };
+
+            Onekey: if (colFINAL < width && keypressed != "del" && keypressed != "enter") {
+                for (let d = 0; d < width; d++) {
+                    if (letterwasguessedtrue[0, d] == 0) {
+                        let CurrentTile = document.getElementById(row.toString + '-' + d.toString());
+                        CurrentTile.innerText = keypressed;
+                        colFINAL += 1;
+                        letterwasguessedtrue[0, d] = 2;
+                        break Onekey;
+                    }
+                }
+            }
+        };
+    };
+};
+
+
