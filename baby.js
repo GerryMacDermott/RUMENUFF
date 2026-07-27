ca4d76c13b27b1785b5854bda7ecb1fb7d0d59e0
GerryMacDermott <41880873+GerryMacDermott@users.noreply.github.com>
2026-07-27 22:30:34 +1000

    Update baby.js

--- baby.js
+++ baby.js
@@ -14,7 +14,7 @@
 ///variables
 var day = 1;
 const word = "NOLA";
-const question = "Do,Re,Mi,Fa,So, ,Ti,Do";
+const question = "Do, Re, Mi, Fa, So,  , Ti, Do";
 var height = 1;
 var width = word.length; //length of the word
 var GuessTile = word.length + 1; //allows the creation of the 'guessing tile'
